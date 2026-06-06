ef64b4753440e0121e137201c0561a2c3abfc936
Hw_Jang <janghw94@gmail.com>
2026-06-07 00:49:44 +0900

    chore: update word to 봄비

--- game.js
+++ game.js
@@ -19,9 +19,9 @@ const JAMO_START   = 0x3131;
 const JAMO_END     = 0x3163;
 
 // 단어 목록 — 순서대로 하루씩 출제
-const WORDS_UPDATED = '2026-06-07 12:33';
+const WORDS_UPDATED = '2026-06-07 00:49';
 const WORDS = [
-    '포장',
+    '봄비',
 ];
 
 const MAX = 5;
