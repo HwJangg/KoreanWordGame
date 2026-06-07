ae13264ae9a40b54b7647d0ae7c2ea92484b3655
Hw_Jang <janghw94@gmail.com>
2026-06-07 09:19:40 +0900

    단어 표범, 시간 2026-06-07 09:20

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 // ── 상수 ──────────────────────────────────────────────────────────────────────
 
-const WORDS_UPDATED = '2026-06-07 09:14';
-const LAST_ANSWER   = '식사';
+const WORDS_UPDATED = '2026-06-07 09:20';
+const LAST_ANSWER   = '자판';
 const WORDS = [
-    '자판',
+    '표범',
 ];
 
 const MAX = 5;
